fixed energy calculation, still have some issues in other memory selection

train_EBM.py
@@ -1,5 +1,4 @@
 import argparse
-from distutils import core
 import os
 import torch
 import numpy as np
@@ -90,7 +89,7 @@ def train_one_epoch(args,
                                        replacement=False)
         memory_loader = DataLoader(dataset=coresets,
                                 sampler=memory_sampler,
-                                batch_size=int(np.min([10*task_num, 64])),
+                                batch_size=32,
                                 drop_last=False)
     for it, sample in enumerate(pbar):
         optimizer.zero_grad()
@@ -106,14 +105,15 @@ def train_one_epoch(args,
                 y = sample[2]+(10*(task_num-1))
                 y = y.to(device)
     
-            
-        joint_targets = get_target(task_class_set, y).to(device)
-        y_ans_idx     = get_ans_idx(list(task_class_set), y).to(device)        
-        energy        = torch.empty(0).to(device)
-        for cl in range(joint_targets.shape[1]):
-            en, _  = model(x, joint_targets[:,cl])
-            energy = torch.cat((energy, en.reshape(-1,1)), dim=1)
-                    
+        
+        joint_targets = get_target(task_class_set, y).to(device).long()
+        # print(task_class_set)
+        # task_class_set_tensor = torch.tensor(list(task_class_set))
+        # joint_targets = task_class_set_tensor.view(1, -1).expand(len(y), len(task_class_set_tensor)).to(device).long()
+        y_ans_idx     = get_ans_idx(task_class_set, y).to(device)
+        # print('outside the model: ', joint_targets.shape)
+        # print(f"epoch: {epoch}, joint_targets : {joint_targets.shape}")
+        energy = model(x, joint_targets)
         if args.criterion   == 'nll_energy':
             if args.use_memory:
                 loss = criterion(energy=energy,
@@ -131,17 +131,13 @@ def train_one_epoch(args,
         train_answers += calculate_answer(energy, y_ans_idx)
         total_len     += y.shape[0]
             
-        if args.use_memory and len(coresets) != 0:
+        if args.use_memory and len(coresets) != 0 and task_num != 1:
             mem_sample = next(iter(memory_loader))
             mem_x = mem_sample[0].to(device)
             mem_y = mem_sample[1].to(device)
             mem_joint_targets = get_target(task_class_set, mem_y).to(device) 
-            mem_y_ans_idx     = get_ans_idx(list(task_class_set)[:-1*args.num_classes//args.num_tasks], mem_y).to(device)
-            mem_energy = torch.empty(0).to(device)
-            for cl in range(mem_joint_targets.shape[1]):
-                mem_en, _ = model(mem_x, mem_joint_targets[:,cl])
-                mem_energy = torch.cat((mem_energy, mem_en.reshape(-1, 1)), dim=1)
-                
+            mem_y_ans_idx     = get_ans_idx(task_class_set, mem_y).to(device)
+            mem_energy = model(mem_x, mem_joint_targets)    
             mem_loss = criterion(energy=mem_energy, 
                                  y_ans_idx=mem_y_ans_idx,
                                  task_class_set=task_class_set,
@@ -159,7 +155,7 @@ def train_one_epoch(args,
         pbar.set_description(desc)
 
         if args.use_memory and epoch == args.epoch and args.learning_mode == 'offline':
-            memory_in_epoch = accumulate_candidate(args.memory_option, model, energy, memory_x, memory_y, memory_energy, x, y)
+            memory_in_epoch = accumulate_candidate(args.memory_option, model, energy, memory_x, memory_y, memory_energy, joint_targets, y_ans_idx, x, y)
         elif args.use_memory and args.learning_mode == 'online':
             for i in range(args.num_classes // args.num_tasks): # class per task만큼 순회
                 cl = torch.tensor(list(task_class_set)[-1*(args.num_classes // args.num_tasks):])[i] # i번째에 해당하는 class (i : task class set의 index, cl : index에 해당하는 class)
@@ -173,7 +169,8 @@ def train_one_epoch(args,
                     new_candidates_i, new_memory_energy = online_bin_based_sampling(model=model, 
                                                                                     memory_size=args.memory_size, 
                                                                                     class_per_tasks=args.num_classes//args.num_tasks,
-                                                                                    candidates=(candidates[0][i], candidates[1][i]), 
+                                                                                    candidates=(candidates[0][i], candidates[1][i]),
+                                                                                    task_class_set=task_class_set, 
                                                                                     x=x_cur, 
                                                                                     y=y_cur,
                                                                                     device=device)
@@ -262,13 +259,13 @@ def test_by_task(args,
             y = y.to(device)
         
         joint_targets = get_target(total_class_set, y).to(device)
-        energy = torch.empty(0).to(device)
-        rep = torch.empty(0)
-        for cl in range(len(class_set)):
-            en, r = model(x, joint_targets[:,cl])
-            energy = torch.cat((energy, en.detach()), dim=-1) # |bxc|
-            rep = torch.cat((rep, torch.unsqueeze(r.detach().cpu(), 0)), dim=0) # |bx1024xc|
-        
+        # energy = torch.empty(0).to(device)
+        # rep = torch.empty(0)
+        # for cl in range(len(class_set)):
+        #     en, r = model(x, joint_targets[:,cl])
+        #     energy = torch.cat((energy, en.detach()), dim=-1) # |bxc|
+        #     rep = torch.cat((rep, torch.unsqueeze(r.detach().cpu(), 0)), dim=0) # |bx1024xc|
+        energy = model(x, joint_targets)
         y = y.detach().cpu()
         energy = energy.detach().cpu() 
         pred_energy, pred_index = torch.min(energy, dim=1) # lowest energy among classes, prediction
@@ -281,10 +278,10 @@ def test_by_task(args,
         confused_energy = torch.cat((confused_energy, confused_pred), dim=0)
         
         ###### get prediction & intermediate representation
-        gt_rep = rep[y, np.arange(len(y)), :]
-        pred_rep = rep[pred_class,np.arange(len(pred_class)),:]
-        reps = torch.cat((reps, gt_rep), dim=0)
-        pred_reps = torch.cat((pred_reps, pred_rep))
+        # gt_rep = rep[y, np.arange(len(y)), :]
+        # pred_rep = rep[pred_class,np.arange(len(pred_class)),:]
+        # reps = torch.cat((reps, gt_rep), dim=0)
+        # pred_reps = torch.cat((pred_reps, pred_rep))
         if i == 0:
             pred_energies = pred_energy
             pred_indices = pred_index.detach().cpu()
@@ -441,7 +438,7 @@ if __name__ == "__main__":
                                                                                        'high_energy', 'min_score', 'max_score',\
                                                                                        'confused_pred', 'representation', 'bin_based'))
     parser.add_argument('--img_size', type=int, default=32)
-    parser.add_argument('--lam', type=float, default=0.5, help='term for balancing current loss and memory loss')
+    parser.add_argument('--lam', type=float, default=1.0, help='term for balancing current loss and memory loss')
     parser.add_argument('--num_channels', type=int, default=3)
     parser.add_argument('--memory_size', type=int, default=20)
     parser.add_argument('--save_confusion_fig', default=False, action='store_true')
@@ -449,7 +446,7 @@ if __name__ == "__main__":
     parser.add_argument('--save_gt_fig', default=False, action='store_true')
     parser.add_argument('--save_pred_tsne_fig', default=False, action='store_true')
     parser.add_argument('--save_pred_tsne_with_gt_label_fig', default=False, action='store_true')
-    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--checkpoint_path', type=str, default='./checkpoint')
     parser.add_argument('--checkpoint', type=str, default='')
     parser.add_argument('--wandb', default=True, action='store_true')